24e250ccece5b72ee26a144d2157f29803606223
manee2k6 <manee2k6@gmail.com>
2018-05-24 10:33:36 +0530

    modified common class for bala

--- src/main/java/com/cd/common/CommonClass.java
+++ src/main/java/com/cd/common/CommonClass.java
@@ -5,7 +5,7 @@ public class CommonClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("welcome to common class...");
+		System.out.println("Welcome Bala for Java project...");
 
 	}
 
